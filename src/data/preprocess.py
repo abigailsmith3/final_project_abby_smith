03024872692f5a55e26d4824214a117d679f051f
abigailsmith3 <abigail_smith3@taylor.edu>
2025-12-02 08:48:59 -0500

    data cleaning
    Updated the clean_dataset function to handle missing values more effectively and changed the dataset loading and saving paths.

--- src/data/preprocess.py
+++ src/data/preprocess.py
@@ -5,17 +5,44 @@ from .load_data import load_dataset
 
 def clean_dataset(df: pd.DataFrame) -> pd.DataFrame:
     """Clean the dataset by dropping rows with missing values."""
-    return df.dropna().copy()
+    tenureMed = df['Tenure'].median()
+    df['Support Calls'] = df['Support Calls'].replace("none",0)
+    df['Tenure'] = df['Tenure'].replace(np.nan,tenureMed)
+    
+    #change this so that the median of support calls is the same as the first one
+    df['Support Calls'] = df['Support Calls'].fillna(int(df['Support Calls'].median()))
+    df['Last Interaction'] = df['Last Interaction'].fillna(df['Last Interaction'].median())
+    clean_df = df.fillna(0)
+
+    
+    return clean_df
 
 
 if __name__ == "__main__":
+    #cleaning data
+
     # Load the raw dataset
-    raw = load_dataset("data/raw/card_transdata.csv")
+    rawData = load_dataset("data/raw/data.csv")
     # Clean the dataset
-    cleaned = clean_dataset(raw)
+    cleanedData = clean_dataset(rawData)
     # Ensure the processed directory exists
     os.makedirs("data/processed", exist_ok=True)
     # Save the cleaned data
-    processed_path = "data/processed/card_transdata_clean.csv"
-    cleaned.to_csv(processed_path, index=False)
-    print(f"Cleaned data saved to {processed_path}")
+    processed_path_data = "data/processed/clean_data.csv"
+    cleanedData.to_csv(processed_path_data, index=False)
+
+    #cleaning test
+    
+    # Load the raw dataset
+    rawTest = load_dataset("data/raw/test.csv")
+    # Clean the dataset
+    cleanedTest = clean_dataset(rawTest)
+    # Ensure the processed directory exists
+    os.makedirs("data/processed", exist_ok=True)
+    # Save the cleaned data
+    processed_path_test = "data/processed/clean_test.csv"
+    cleanedTest.to_csv(processed_path_test, index=False)
+    
+    print(f"Cleaned data saved to {processed_path_data}")
+    print(f"Cleaned test data saved to {processed_path_test}")
+
