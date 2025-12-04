1db4726ec32aebbf56ad5704cb6bd8888fcfe16f
abigailsmith3 <abigail_smith3@taylor.edu>
2025-12-04 10:53:37 -0500

    Change KNN model to use 11 neighbors

--- src/models/knn_model.py
+++ src/models/knn_model.py
@@ -3,7 +3,7 @@ from sklearn.neighbors import KNeighborsClassifier
 
 
 def train_knn_model(X_train: pd.DataFrame, y_train: pd.Series) -> KNeighborsClassifier:
-    """Train and return a 3-NN classifier."""
-    model = KNeighborsClassifier(n_neighbors=3)
-    model.fit(X_train, y_train)
-    return model
+    """Train and return a 11-NN classifier."""
+    knn_model = KNeighborsClassifier(n_neighbors=11)
+    knn_model.fit(X_train, y_train)
+    return knn_model
